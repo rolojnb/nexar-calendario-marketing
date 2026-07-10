5a16939c8891135a9162510cf70eff97825470d5
Rolo JN Beltrán <mailxsuscripcion@gmail.com>
2026-07-09 22:16:19 -0300

    Implement modular data sources

---
+++ services/data_sources/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from services.data_sources.base import BusinessDataContext, ProductData, empty_business_context
+from services.data_sources.registry import load_business_context
+
+
+__all__ = [
+    "BusinessDataContext",
+    "ProductData",
+    "empty_business_context",
+    "load_business_context",
+]
